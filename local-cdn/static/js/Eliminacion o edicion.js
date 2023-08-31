9170b6b116f1492454351f007fc79017c0e30981
Kevin Coronado <kevingalileo100@gmail.com>
2023-08-31 12:02:10 -0500

    Agregando reglas a .gitignore

--- local-cdn/static/js/Eliminacion o edicion.js
+++
@@ -1,9 +0,0 @@
-const documentoInput = document.getElementById("ID_C_DOCUMENTO");
-    const editarLink = document.getElementById("editarLink");
-    const eliminarLink = document.getElementById("eliminarLink");
-
-    documentoInput.addEventListener("input", () => {
-        const valorDocumento = documentoInput.value;
-        editarLink.href = `/api/edicionstd/${valorDocumento}`;
-        eliminarLink.href = `/api/eliminacionstd/${valorDocumento}`;
-    });
