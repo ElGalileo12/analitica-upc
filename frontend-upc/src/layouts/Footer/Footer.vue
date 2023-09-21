88154ec513c280b20637170e1019bf583c95d241
AndresG1237 <andrescami1237@gmail.com>
2023-09-21 13:13:52 -0500

    Cambios en nav footer y agrege home pages

---
+++ frontend-upc/src/layouts/Footer/Footer.vue
@@ -0,0 +1,107 @@
+<template>
+<div class="sectionfour">
+      <div class="col-lg-6 mx-5">
+        <h1>Contactanos</h1>
+        <p class="parrafo_1">Estudiante: Andres Camilo Reyes Galindo</p>
+        <p class="parrafo">Correo: acamiloreyes@unicesar.edu.co</p>
+      </div>
+      <div class="col-lg-6">
+        <ul class="nav justify-content-center">
+          <i class="fa-solid fa-house" style="color: #ffffff; margin-top: -21px;"></i>
+          <li class="nav-item"><a href="/home" class="nav-link px-2 text-body-secondary">Home</a></li>\
+          <i class="fa-solid fa-magnifying-glass" style="color: #ffffff; margin-top: -21px"></i>
+          <li class="nav-item"><a href="Home#sectiontwo" class="nav-link px-2 text-body-secondary">Features</a></li>
+          <i class="fa-solid fa-building-columns" style="color: #ffffff; margin-top: -21px"> </i>
+          <li class="nav-item"><a href="https://www.unicesar.edu.co" class="nav-link px-2 text-body-secondary">University</a></li>
+          <i class="fa-solid fa-people-group" style="color: #ffffff;margin-top: -21px;"></i>
+          <li class="nav-item"><a href="Home#Quienes" class="nav-link px-2 text-body-secondary">About us</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="sectionfive">
+      <p class="text-center text-body-secondary border-top pt-3 my-3">© 2023 Company, Inc</p>
+    </div>
+</template>
+<style>
+.sectionfour{
+    text-align: center;
+    padding-top: 1rem;
+    display: flex;
+    align-items: center;
+    background-color: #306de0;
+    height: 30vh;
+    padding-right: 150px;
+}
+
+.sectionfour li a{
+    margin-top: -2rem;
+    font-family: "Handlee", cursive;
+    font-weight: bold;
+    display: block;
+    color: #fdfdfd;
+    text-transform: none;
+    font-size: 18px;
+    
+}
+.sectionfour p{
+    font-family: "Handlee", cursive;
+    font-weight: bold;
+    display: block;
+    color: #fdfdfd;
+    text-transform: none;
+    font-size: 18px;
+    padding-top: 10px;
+    
+}
+.sectionfour li a:hover{
+    color: #889fca; 
+}
+
+.sectionfour h1{
+    margin-top: -4rem;
+    color: #f7f7f7;
+    font-weight: bold;
+    font-family: "Handlee", cursive;
+    font-size: 40px;
+
+}
+
+.sectionfour h1::before{
+    content: "\f0e0"; /* Código Unicode del icono de corazón en Font Awesome */
+    font-family: "Font Awesome 5 Free"; /* Nombre de la fuente de Font Awesome */
+    margin-top: 11px; /* Añade margen a la derecha del icono para separarlo del texto */
+    margin-right: 5px;
+}
+
+.sectionfour p.parrafo_1::before{
+    content: "\f007"; /* Código Unicode del icono de corazón en Font Awesome */
+    font-family: "Font Awesome 5 Free"; /* Nombre de la fuente de Font Awesome */
+    margin-top: 11px; /* Añade margen a la derecha del icono para separarlo del texto */
+    margin-right: 5px;
+    margin-bottom: 0px;
+}
+
+.sectionfour p.parrafo::before{
+    content: "\e4e8"; /* Código Unicode del icono de corazón en Font Awesome */
+    font-family: "Font Awesome 5 Free"; /* Nombre de la fuente de Font Awesome */
+    margin-top: 11px; /* Añade margen a la derecha del icono para separarlo del texto */
+    margin-right: 5px;
+    margin-bottom: 0px;
+}
+
+
+.sectionfour p.parrafo{
+    font-family: "Nunito",sans-serif;
+    color: #f8f8f8;
+    margin-top: -1rem;
+    margin-bottom: 0px;
+
+}
+
+.sectionfive{
+    color: #ffffff;
+    background: #306de0;
+    margin-top: -61px;
+    margin-bottom: -18px;
+}
+</style>
--- frontend-upc/src/pages/Home.view.vue
+++ frontend-upc/src/pages/Home.view.vue
@@ -1,21 +1,166 @@
-<script setup>
-import { defineComponent } from "vue";
-import PersonalForm from "../components/forms/personal.form.vue";
-import Separador from "../components/things/separador.things.vue";
-
-defineComponent({
-  components: {
-    PersonalForm,
-    Separador,
-  },
-});
-</script>
-
 <template>
-  <div class="m-8">
-    <PersonalForm></PersonalForm>
-    <Separador></Separador>
+<div class="container-fluid">
+    <div class="row align-items-center"></div>
+            <div class="col-lg-6 text-justify text-lg-left">
+              <h1 class="title">Proyecto institucional</h1>
+                <p>
+                  Este software, desarrollado especialmente para asistir a los investigadores y profesionales de la educación,
+                  ofrece una herramienta extraordinariamente eficaz para comprender en profundidad los múltiples factores que
+                  contribuyen a la deserción de los estudiantes en el prestigioso programa de Ingeniería Electrónica de la
+                  Universidad Popular del César.
+                </p>
+            </div>
+            <div class="col-lg-6 text-center text-lg-right">
+                  <img src="../assets/g1.png"  alt="">
+            </div>
+    </div>
+
+  <!-- Facilities Start -->
+    <div class="sectiontwo pb-3" id="sectiontwo">
+        <h1></h1>
+          <div class="row">
+              <div class="col-lg-4 col-md-6 pb-1">
+                  <div class="d-flex bg-light  border-top rounded mb-4" style="padding: 32px;">
+                      <div class="pl-4">
+                          <h4>Compromiso con la Excelencia Educativa </h4>
+                          <p class="m-0">Buscamos la mejora constante en la educación.</p>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-4 col-md-6 pb-1">
+                  <div class="d-flex bg-light  border-top rounded mb-4" style="padding: 30px;">
+                      <div class="pl-4">
+                          <h4>Accesibilidad y Usabilidad</h4>
+                          <p class="m-3">Nuestra interfaz es amigable y fácil de usar.</p>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-4 col-md-6 pb-1">
+                  <div class="d-flex bg-light  border-top rounded mb-4" style="padding: 34px;">
+                      <div class="pl-4">
+                        <h4>Análisis de Datos Avanzados </h4>
+                        <p class="m-0">Utilizamos técnicas estadísticas y algoritmos sofisticados.</p>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-4 col-md-6 pb-1">
+                  <div class="d-flex bg-light  border-top rounded mb-4" style="padding: 30px;">
+                      <div class="pl-4">
+                          <h4>Enfoque Basado en Datos</h4>
+                          <p class="m-0">Tomamos decisiones informadas mediante análisis detallados.</p>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-4 col-md-6 pb-1">
+                  <div class="d-flex bg-light  border-top rounded mb-4" style="padding: 30px;">
+                      <div class="pl-4">
+                          <h4>Enfoque Multidimensional</h4>
+                          <p class="m-0">Analizamos factores diversos que afectan la deserción.</p>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-4 col-md-6 pb-1">
+                  <div class="d-flex bg-light  border-top rounded mb-4" style="padding: 13px;">
+                      <div class="pl-3">
+                          <h4>Mejora en la Retención Estudiantil</h4>
+                          <p class="m-3">Contribuye a aumentar la retención y el éxito académico de los estudiantes.</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+
+
+  <section class="sectionthree" id="Quienes">
+    <div class="recuadro bg-light rounded" id="recuadro" style="height: 500px; margin-left: 30px; margin-right: 30px;">
+      <h1 class="text-center">Quienes Somos?</h1>
+      <p class="pb-3">
+        Somos un grupo de estudiantes apasionados de Ingeniería Electrónica de la Universidad Popular del César, unidos por un propósito común: contribuir de manera significativa en la lucha contra la deserción estudiantil. Con el apoyo invaluable de la Vicerrectoría Académica y el Semillero de Tratamiento de Señales, hemos formado un equipo de 10 estudiantes comprometidos, respaldados por el conocimiento y la experiencia del Ingeniero José Ramón Inglesias y la Ingeniera Omaira Tapia.
+        Nuestra misión es utilizar nuestros conocimientos y habilidades en ingeniería para desarrollar un proyecto institucional que aborde de manera efectiva el desafío de la deserción estudiantil. Creemos que la educación es la clave para un futuro mejor, y estamos decididos a hacer nuestra parte para garantizar que cada estudiante tenga la oportunidad de alcanzar su máximo potencial.
+        Juntos, estamos trabajando en la implementación de un enfoque innovador y basado en datos para comprender y prevenir la deserción estudiantil en nuestra universidad. A través de análisis de datos avanzados, modelos de pronóstico y estrategias de retención estudiantil, estamos comprometidos en mejorar la calidad de la educación y proporcionar un entorno enriquecedor para todos los que forman parte de nuestra comunidad académica.
+        Nuestra visión es ser un faro de cambio en la lucha contra la deserción estudiantil, no solo en nuestra institución, sino también como un ejemplo inspirador para otras instituciones educativas.
+      </p>
   </div>
+  </section>
 </template>
 
-<style></style>
+<style>
+
+
+.container-fluid{
+    height: 80vh;
+}
+.container-fluid, .sectiontwo, .sectionthree{
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15pxpx;
+    margin-right: auto;
+    margin-left: auto;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    color: aliceblue;
+    background:  url(../assets/cool-background.jpg) center no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+}
+
+.container-fluid h1{
+    font-family: "Handlee", cursive;
+    font-weight: bold;
+    font-size: 60px;
+    padding-left: 70px;
+}
+.container-fluid p{
+    font-weight: bold;
+    padding-left: 50px;
+}
+
+.sectiontwo div.d-flex, .sectionthree div.recuadro{
+    box-shadow: 0 .125rem .125rem rgba(0,0,0,0.5)!important;
+}
+
+.sectiontwo h4{
+    color: #306de0;
+    font-weight: bold;
+    font-family: "Handlee", cursive;
+    font-size: 25px;
+    text-align: center;
+}
+
+.sectiontwo p{
+    color: #000000;
+    font-size: 15px;
+    text-align: center;
+}
+
+.sectionthree{
+    text-align: justify;
+    padding-top: 20px;
+    color: rgb(255, 255, 255);
+    text-transform: none;
+    /**background:  url(img/cool-background.png) center no-repeat;
+    background-attachment: fixed;
+    background-size: cover;**/
+    height: 100vh; 
+    }
+
+.sectionthree h1{
+    color: #3570df;
+    padding-top: 3rem;
+    font-family: "Handlee", cursive;
+    font-weight: bold;
+    font-size: 70px;
+    margin-bottom: 0.1rem;
+    padding-top: 20px;
+}
+
+.sectionthree p{
+    color: #000000;
+    margin-left: 10rem;
+    margin-right: 10rem;
+    padding-bottom: 15rem;
+}
+</style>
